dec956b535f03dcec37eb462c6f54ff6e581f6a8
Balint Molnar <balintmolnar91@gmail.com>
2023-11-20 18:46:38 +0100

    WIP5

--- .github/scripts/create_repos.sh
+++ .github/scripts/create_repos.sh
@@ -16,18 +16,18 @@ main() {
   DEB_DISTS_COMPONENTS="${DEB_DISTS}/${COMPONENTS:-main}/binary-all"
   GPG_TTY=""
   export GPG_TTY
-  if release=$(curl -fqs https://api.github.com/repos/${repo}/releases/latest)
+  if release=$(curl -fqs https://api.github.com/repos/baluchicken/aptrepo/releases/latest)
   then
     tag="$(echo "$release" | jq -r '.tag_name')"
     deb_file="$(echo "$release" | jq -r '.assets[] | select(.name | endswith(".deb")) | .name')"
-    echo "Parsing repo $repo at $tag"
+    echo "Parsing repo /baluchicken/aptrepo at $tag"
     if [ -n "$deb_file" ]
     then
       GOT_DEB=1
       mkdir -p "$DEB_POOL"
       pushd "$DEB_POOL" >/dev/null
       echo "Getting DEB"
-      wget -q "https://github.com/${repo}/releases/download/${tag}/${deb_file}"
+      wget -q "https://github.com/baluchicken/aptrepo/releases/download/${tag}/${deb_file}"
       popd >/dev/null
     fi
   fi
